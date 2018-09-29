6658877f815a094ca51a1e78971f3f6b4259def9
Victor Domiciano <victor.dm3@gmail.com>
2018-09-29 02:13:09 -0300

    Added dive
    Signed off by: @rilifon

--- project/player/player.gd
+++ project/player/player.gd
@@ -11,6 +11,10 @@ var speed = 200
 var dir
 var last_trail_pos = Vector2(0, 0)
 var trail = TRAIL.instance()
+var diving = false
+var can_dive = true
+var dive_cooldown = 3
+var dive_duration = 1
 
 func _ready():
 	dir = Vector2(1, 0)
@@ -25,7 +29,8 @@ func _physics_process(delta):
 	sprite.rotation = dir.angle()
 	area.rotation = dir.angle()
 	if self.position.distance_to(last_trail_pos) > 2 * trail.get_node('Area2D/CollisionShape2D').get_shape().radius:
-		create_trail(self.position)
+		if not diving:
+			create_trail(self.position)
 
 func create_trail(pos):
 	var trail = TRAIL.instance()
@@ -40,5 +45,31 @@ func _on_Area2D_area_exited(area):
 
 func _on_Area2D_area_entered(area):
 	var _trail = area.get_parent()
-	if _trail.is_in_group('trail') and _trail.can_collide:
+	if _trail.is_in_group('trail') and _trail.can_collide and not diving:
 		self.queue_free()
+
+func _input(event):
+	if event.is_action_pressed('ui_accept') and can_dive:
+		dive()
+
+func dive():
+	var timer = Timer.new()
+	can_dive = false
+	diving = true
+	timer.wait_time = dive_duration
+	area.visible = false
+	timer.connect('timeout', self, 'emerge')
+	timer.start()
+	self.add_child(timer)
+
+func emerge():
+	var timer = Timer.new()
+	diving = false
+	timer.wait_time = dive_cooldown
+	area.visible = true
+	timer.connect('timeout', self, 'enable_diving')
+	timer.start()
+	self.add_child(timer)
+
+func enable_diving():
+	can_dive = true
