ff7219eb6bfa9454303f2e16868e8b8acfd2428b
Victor Domiciano <victor.dm3@gmail.com>
2018-09-29 13:04:36 -0300

    Add scalable obstacles

--- project/wall/obstacle.gd
+++ project/wall/obstacle.gd
@@ -1,10 +1,9 @@
-extends Sprite
+extends Node2D
 
-export (Vector2) var extents
+onready var sprite = $Sprite
+onready var area_polygon = $Area2D/CollisionPolygon2D
 
 func _ready():
-	if extents:
-		var shape = RectangleShape2D.new()
-		shape.extents = extents
-		$Area2D/CollisionShape2D.shape = shape
-
+	var sprite_size = Vector2(abs(area_polygon.polygon[1].x - area_polygon.polygon[0].x), abs(area_polygon.polygon[2].y - area_polygon.polygon[1].y))
+	sprite.scale = sprite_size/sprite.texture.get_size()
+	sprite.position = area_polygon.polygon[0] + sprite_size/2
