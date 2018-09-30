cab1dce79ef0c0d102cac91d1a3abd1cb2d7f9a0
rilifon <pudimdefrutas@gmail.com>
2018-09-30 12:03:59 -0300

    Removed shockwave

--- project/player/player.gd
+++ project/player/player.gd
@@ -98,7 +98,6 @@ func _on_Area2D_area_entered(area):
 			_queue_free(true)
 
 func _queue_free(player_collision=false):
-	$Shockwave.visible = true
 	$Scream.play()
 	$Explosion.emitting = true
 	$Explosion2.emitting = true
