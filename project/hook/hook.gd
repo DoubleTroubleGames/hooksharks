de0524698d0047bbc183abf12354d6d3868cb749
Andre Ferrari Moukarzel <andremoukarzel@hotmail.com>
2019-01-24 19:31:55 -0200

    Cleanup
    - Renamed Area2D object
    - Transformed a few lines into functions, for easier understanding

--- project/hook/hook.gd
+++ project/hook/hook.gd
@@ -3,7 +3,7 @@ extends Node2D
 var speed = 700
 var acc = 500
 var retracting = false
-var kill_distance = 20
+var kill_distance = 20 # distance to delete hook when retracting
 
 onready var camera = get_node('../../Camera2D')
 onready var hook_clink = get_node('../../HookClink')
@@ -26,31 +26,43 @@ func _physics_process(delta):
 		dir = (player.position - self.position).normalized()
 		position += dir * (delta * speed * 2)
 		if position.distance_to(player.position) <= kill_distance:
-			rope.queue_free()
-			player.hook = null
-			self.queue_free()
+			free_hook()
 
-func _on_Area2D_area_entered(area):
+func _on_HookArea_area_entered(area):
 	var object = area.get_parent()
 	if object.is_in_group('hook') and not retracting:
-		player.hook.retract()
-		player.map.blink_screen()
-		Bgm.get_node('HookHitHook').play()
-		camera.add_shake(.8)
-		hook_clink.position = (object.position + self.position)/2
-		hook_clink.emitting = true
+		hookHitHook()
 	elif object.is_in_group('player') and object != player and not retracting:
-		if not object.stunned and not object.diving:
-			$BloodParticles.emitting = true
-			stop_at = object
-			object.hook_collision(self)
-			Bgm.get_node('HookHitPlayer').play()
-			camera.add_shake(.7)
+		hookHitShark()
 	elif object.is_in_group('wall') and not retracting:
-		$WallParticles.emitting = true
-		camera.add_shake(.3)
-		has_collided = true
-		Bgm.get_node('HookHitWall').play()
+		hookHitWall()
+
+func hookHitHook():
+	retract()
+	player.map.blink_screen()
+	Bgm.get_node('HookHitHook').play()
+	camera.add_shake(.8)
+	hook_clink.position = (object.position + self.position)/2
+	hook_clink.emitting = true
+
+func hookHitShark():
+	if not object.stunned and not object.diving:
+		$BloodParticles.emitting = true
+		stop_at = object
+		object.hook_collision(self)
+		Bgm.get_node('HookHitPlayer').play()
+		camera.add_shake(.7)
+
+func hookHitWall():
+	$WallParticles.emitting = true
+	camera.add_shake(.3)
+	has_collided = true
+	Bgm.get_node('HookHitWall').play()
 
 func retract():
 	retracting = true
+
+func free_hook():
+	rope.queue_free()
+	player.hook = null
+	self.queue_free()
