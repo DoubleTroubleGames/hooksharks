35b90c102ec4f7333e82483870da2bf0b6bff7f8
rilifon <pudimdefrutas@gmail.com>
2019-01-31 12:05:19 -0200

    Simple race level

--- project/race-mode/stages/StagesDb.gd
+++ project/race-mode/stages/StagesDb.gd
@@ -2,15 +2,7 @@ extends Node
 
 const STAGES = [
 		preload('res://race-mode/stages/Stage1.tscn'),
-		preload('res://race-mode/stages/Stage2.tscn'),
-		preload('res://race-mode/stages/Stage3.tscn'),
-		preload('res://race-mode/stages/Stage4.tscn'),
-		preload('res://race-mode/stages/Stage5.tscn'),
-		#preload('res://race-mode/stages/Stage6.tscn'),
-		preload('res://race-mode/stages/Stage7.tscn'),
-		preload('res://race-mode/stages/Stage8.tscn'),
-		preload('res://race-mode/stages/Stage9.tscn'),
-		preload('res://race-mode/stages/Stage10.tscn')]
+		preload('res://race-mode/stages/Stage2.tscn')]
 
 func get_random_stage():
 	return STAGES[1 + (randi() % (STAGES.size() - 1))]
