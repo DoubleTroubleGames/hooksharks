edb44b266948d37803cff3bdf097cf73042dc050
Detril <andremouka@hotmail.com>
2019-01-31 11:01:13 -0200

    Adapt to conventions
    Signed-off-by: Detril <andremouka@hotmail.com>

--- project/hud/Hud.gd
+++ project/hud/Hud.gd
@@ -109,4 +109,4 @@ func _on_Restart_pressed():
 	get_tree().reload_current_scene()
 
 func _on_Quit_pressed():
-	get_tree().change_scene("res://main_menu/main_menu.tscn")
+	get_tree().change_scene("res://main-menu/MainMenu.tscn")
