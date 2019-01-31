a8a315a105f9e3a8f4c276171b38ec1c8af90879
Detril <andremouka@hotmail.com>
2019-01-31 11:18:13 -0200

    Compartmentalize Arena
    Signed-off-by: Detril <andremouka@hotmail.com>

--- project/main-menu/MainMenu.gd
+++ project/main-menu/MainMenu.gd
@@ -1,11 +1,17 @@
 extends Control
 
 func _ready():
-	$Start.grab_focus()
+	$StartRace.grab_focus()
 
-func _on_Start_pressed():
+
+func _on_StartRace_pressed():
+	pass # replace with function body
+
+
+func _on_StartArena_pressed():
 	BGM.get_node('Click').play()
 	get_tree().change_scene("res://Main.tscn")
+
 
 func _on_Quit_pressed():
 	get_tree().quit()
