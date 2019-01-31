18fb0603eca49754b3d221492fc4cf3fb1637590
Detril <andremouka@hotmail.com>
2019-01-31 12:20:14 -0200

    Add tool to change size dynamically
    Signed-off-by: Detril <andremouka@hotmail.com>

--- project/race-mode/FinishLine.gd
+++ project/race-mode/FinishLine.gd
@@ -1,8 +1,8 @@
+tool
+
 extends Node2D
 
-
-func _ready():
-	$Direction.hide()
+export (int)var size = 300 setget set_size
 
 
 func _on_Area2D_area_entered(area):
@@ -13,3 +13,10 @@ func _on_Area2D_area_entered(area):
 		for child in players.get_children():
 			if child.is_in_group('player') and child != winner:
 				child._queue_free()
+
+
+func set_size(s):
+	size = s
+	var shape = RectangleShape2D.new()
+	shape.set_extents(Vector2(10, size))
+	$Area2D/CollisionShape2D.set_shape(shape)
