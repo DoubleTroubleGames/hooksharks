3d85bfe9c4a21fee9e4e9a2ddb8ddd7aa0787f11
rilifon <pudimdefrutas@gmail.com>
2019-01-31 12:10:30 -0200

    Disable trail on race mode

--- project/player/Player.gd
+++ project/player/Player.gd
@@ -12,7 +12,11 @@ const EXPLOSIONS = [preload("res://player/explosion/1.png"), preload("res://play
 
 export (int, -1, 3)var id
 export (Vector2)var initial_dir = Vector2(1, 0)
+
 export (String, "Keyboard_mouse", "Gamepad") var input_type = "Keyboard_mouse"
+
+export (bool)var create_trail = true
+
 
 onready var arrow = $Arrow
 onready var bar = $DiveCooldown/Bar
@@ -70,7 +74,7 @@ func _physics_process(delta):
 
 	rotation = speed2.angle()
 
-	if self.position.distance_to(last_trail_pos) > 2 * trail.get_node('Area2D/CollisionShape2D').get_shape().radius:
+	if self.create_trail and self.position.distance_to(last_trail_pos) > 2 * trail.get_node('Area2D/CollisionShape2D').get_shape().radius:
 		if not diving:
 			create_trail(self.position)
 
