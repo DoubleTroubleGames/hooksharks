efb2eb6b97d945225c561cd922861ec91f10b5be
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2019-02-07 13:00:36 -0200

    Makes SplitScreen's code generic for any number of Viewports

--- project/race-mode/SplitScreen.gd
+++ project/race-mode/SplitScreen.gd
@@ -1,32 +1,34 @@
 extends Node
 
-onready var viewport1 = $Viewports/ViewportContainer1/Viewport
-onready var viewport2 = $Viewports/ViewportContainer2/Viewport
-onready var camera1 = $Viewports/ViewportContainer1/Viewport/Camera2D
-onready var camera2 = $Viewports/ViewportContainer2/Viewport/Camera2D
 onready var world = $Viewports/ViewportContainer1/Viewport/Race
 
 
 func _ready():
-	viewport2.world_2d = viewport1.world_2d
-	camera1.target = world.get_node("Players/Player1")
-	camera2.target = world.get_node("Players/Player2")
-
-
-func connect_players_to_cameras():
-	var all_players = get_all_players()
+	var all_viewports = get_all_viewports()
 	var all_cameras = get_all_cameras()
+	var all_players = get_all_players()
+	
+	for viewport in all_viewports:
+		viewport.world_2d = all_viewports[0].world_2d
+	for i in range(all_players.size()):
+		all_cameras[i].target = all_players[i]
 
-	for camera in all_cameras:
-		for player in all_players:
-			player.connect("shook_screen", camera, "add_shake")
-		world.get_node("HUD").connect("shook_screen", camera, "add_shake")
+
+func get_all_viewports():
+	var viewports = []
+	
+	for i in range(1, 5):
+		var path = str("Viewports/ViewportContainer", i, "/Viewport")
+		if self.has_node(path):
+			viewports.append(get_node(path))
+	
+	return viewports
 
 
 func get_all_players():
 	var players = []
 	
-	for i in range(1, 4):
+	for i in range(1, 5):
 		var path = str("Players/Player", i)
 		if world.has_node(path):
 			players.append(world.get_node(path))
@@ -37,7 +39,7 @@ func get_all_players():
 func get_all_cameras():
 	var cams = []
 	
-	for i in range(1, 4):
+	for i in range(1, 5):
 		var path = str("Viewports/ViewportContainer", i, "/Viewport/Camera2D")
 		if self.has_node(path):
 			cams.append(get_node(path))
