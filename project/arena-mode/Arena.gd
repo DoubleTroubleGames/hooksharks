75b1c35c4482a30d983cca73fa194c4a9f796aa0
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2019-02-07 10:28:07 -0200

    Continue refactor

--- project/arena-mode/Arena.gd
+++ project/arena-mode/Arena.gd
@@ -84,14 +84,7 @@ func show_round():
 func remove_player(player, is_player_collision):
 	players.erase(player)
 	if players.size() == 1:
-		# var timer = Timer.new()
 		var winner = players[0]
-		# timer.wait_time = 1
-		# self.add_child(timer)
-		# timer.connect('timeout', get_parent(), 'show_round')
-		# timer.one_shot = true
-		# timer.pause_mode = PROCESS
-		# timer.start()
 		if not is_player_collision:
 			var winner_id = get_winner_id(winner)
 			Global.scores[winner_id] += 1
@@ -100,7 +93,7 @@ func remove_player(player, is_player_collision):
 			Global.winner = -1
 		winner.set_physics_process(false)
 		winner.set_process_input(false)
-
+		
 		yield(get_tree().create_timer(SHOW_ROUND_DELAY), "timeout")
 		show_round()
 
@@ -129,7 +122,7 @@ func _on_player_hook_shot(player, direction):
 	
 	BGM.get_node('Harpoon').play()
 	new_hook.rope = rope
-	player.hook = hook
+	player.hook = new_hook
 
 func _on_player_created_trail(trail):
 	$Trail.add_child(trail)
