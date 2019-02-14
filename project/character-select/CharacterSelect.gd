ff2307a76979d2ecc24e2a177fd149f7d53b6cce
LuizGyro <luizfelipe.g@hotmail.com>
2019-02-14 11:56:57 -0200

    Continue Developing Character Select Screen Refactored some logic regarding the states in SelectionBox. Some of the logic is still kinda wonky, maybe it's a good ideia to think about it once more, to determine which scene should have what responsibilities.

--- project/character-select/CharacterSelect.gd
+++ project/character-select/CharacterSelect.gd
@@ -1,4 +1,20 @@
 extends Control
 
 func _ready():
-	pass
+	$GridContainer/SelectionBox1.set_process_input(false)
+	$GridContainer/SelectionBox2.set_process_input(false)
+	$GridContainer/SelectionBox3.set_process_input(false)
+
+func _on_SelectionBox_selected(id):
+	var next_id = RoundManager.get_open_port()
+	get_node(str("GridContainer/SelectionBox", next_id)).set_process_input(true)
+	print(RoundManager.control_map)
+func _on_SelectionBox_unselected(id):
+	var i = 0
+	for port in RoundManager.control_map:
+		if port == null:
+			get_node(str("GridContainer/SelectionBox", i)).set_process_input(false)
+		i += 1
+	var next_id = RoundManager.get_open_port()
+	get_node(str("GridContainer/SelectionBox", next_id)).set_process_input(true)
+	print(RoundManager.control_map)
