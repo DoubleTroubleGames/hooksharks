5a8a294c1f82ace8b2531ffb0ce4ecc72bd963c6
LuizGyro <luizfelipe.g@hotmail.com>
2019-02-14 11:56:57 -0200

    Continue Developing Character Select Screen Refactored some logic regarding the states in SelectionBox. Some of the logic is still kinda wonky, maybe it's a good ideia to think about it once more, to determine which scene should have what responsibilities.

--- project/character-select/SelectionBox.gd
+++ project/character-select/SelectionBox.gd
@@ -1,28 +1,57 @@
 extends Control
 
+signal selected(id)
+signal unselected(id)
+
+const characters = [Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)]
 enum States {UNSELECTED, SELECTED, READY}
 
+export(int, 0, 3) var id = 0
+
 var current_state = States.UNSELECTED
+var current_character_id = 0
 
 
 func _input(event):
+	if not event is InputEventKey and not event is InputEventJoypadButton:
+		return
+	var device = determine_device_type(event)
+	
 	match current_state:
 		UNSELECTED:
 			if event.is_action_pressed("start"):
-				if event is InputEventKey:
-					RoundManager.control_map.append('keyboard')
-				else:
-					RoundManager.control_map.append(event.device)
-				change_state(SELECTED, event.device)
-				print(RoundManager.control_map)
+				if not device in RoundManager.control_map:
+					if(RoundManager.assign_to_port(device)):
+						change_state(SELECTED, device)
+						emit_signal("selected", id)
 		SELECTED:
-			pass
+			if not device == RoundManager.control_map[id]:
+				return
+			if event.is_action_pressed("ui_cancel"):
+				RoundManager.unassign_port(device)
+				change_state(UNSELECTED, device)
+				emit_signal("unselected", id)
+			if event.is_action_pressed("ui_left"):
+				current_character_id -= 1
+			elif event.is_action_pressed("ui_right"):
+				current_character_id += 1
+			current_character_id %= characters.size()
+			$Sprite.set_modulate(characters[current_character_id])
 		READY:
 			pass
 
-func change_state(new_state, device_id):
+func determine_device_type(event):
+	var device
+	if event is InputEventKey:
+		device = 'keyboard'
+	else:
+		device = str("gamepad_", event.device)
+	return device
+	
+
+func change_state(new_state, device):
 	current_state = new_state
 	$State.text = str(new_state)
-	$DeviceId.text = str(device_id)
+	$DeviceId.text = str(device)
 	
 	
