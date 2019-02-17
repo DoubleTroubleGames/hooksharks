fbd18043c4f9727ea60ceecdfbeaefd7dbdb419b
Detril <andremoukarzel@gmail.com>
2019-02-17 12:13:15 -0300

    Add Player texture and Race's camera activation

--- project/Stage.gd
+++ project/Stage.gd
@@ -9,6 +9,7 @@ func setup(GameScene, player_num):
 		
 		Player.set_position(StartPos.position)
 		Player.initial_dir = StartPos.direction
+		Player.get_node("Sprite").set_texture(load("res://player/sprite_sheet_v02.png"))
 		Player.set_name(str("Player", i))
 		Player.id = i - 1
 		GameScene.players.append(Player)
--- project/race-mode/Race.gd
+++ project/race-mode/Race.gd
@@ -1,10 +1,18 @@
 extends "res://Game.gd"
 
-#export (NodePath)var SplitScreen = null
-
-#func _ready():
-#	$Camera2D.set_children($Players.get_children())
+export (NodePath)var SplitScreen = null
 
 func get_cameras():
-#	return get_node(SplitScreen).get_all_cameras()
+	if SplitScreen:
+		return get_node(SplitScreen).get_all_cameras()
 	return [$Camera2D]
+
+func connect_players():
+	for player in players:
+		player.connect("created_trail", self, "_on_player_created_trail")
+		player.connect("hook_shot", self, "_on_player_hook_shot")
+		player.connect("died", self, "remove_player")
+		for camera in Cameras:
+			player.connect("shook_screen", camera, "add_shake")
+	for camera in Cameras:
+		camera.set_children(players)
