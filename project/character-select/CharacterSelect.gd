5bc59a2d849c2cb49698407cc0b5d65fb6449066
LuizGyro <luizfelipe.g@hotmail.com>
2019-02-21 17:43:53 -0300

    Refactor character selection screen Based on @ArthurTui code. Need to remove debug code.

--- project/character-select/CharacterSelect.gd
+++ project/character-select/CharacterSelect.gd
@@ -1,38 +1,49 @@
 extends Control
 
-var ready_num = 0
+onready var boxes = $Boxes.get_children()
+
+var available_characters
 
 func _ready():
-	get_node("GridContainer/SelectionBox0").set_process_input(true)
+	available_characters = $Boxes/SelectionBox0.CHARACTERS.duplicate()
+
+	for i in range(boxes.size()):
+		pass
+		boxes[i].connect("selected", self, "_on_box_selected")
+		boxes[i].connect("unselected", self, "_on_box_unselected")
+		boxes[i].connect("readied", self, "_on_box_readied")
+		boxes[i].set_character(0)
 
 
-func _on_SelectionBox_selected(id):
-	var next_id = RoundManager.get_open_port()
-	get_node(str("GridContainer/SelectionBox", next_id)).set_process_input(true)
+func _input(event):
+	if event.is_action_pressed("ui_start"):
+		for box in boxes:
+			if box.is_closed():
+				box.open_with(event)
+				return
+	elif event.is_action_pressed("ui_cancel"):
+		# Go to previous screen
+		pass
 
 
-# Turns off all empty boxes, then turns on the first empty box it finds
-func _on_SelectionBox_unselected(id):
-	var i = 0
-	for port in RoundManager.control_map:
-		if port == null:
-			get_node(str("GridContainer/SelectionBox", i)).set_process_input(false)
-		i += 1
-	var next_id = RoundManager.get_open_port()
-	get_node(str("GridContainer/SelectionBox", next_id)).set_process_input(true)
+func update_boxes():
+	for box in boxes:
+		box.update_available_characters(available_characters)
 
 
-func _on_SelectionBox_ready(id):
-	ready_num += 1
+func _on_box_selected(character):
+	available_characters.erase(character)
+	update_boxes()
 
 
-func _on_SelectionBox_unready(id):
-	ready_num -= 1
+func _on_box_unselected(character):
+	available_characters.append(character)
+	update_boxes()
 
 
-func _on_SelectionBox_start_game():
-	if ready_num > 1:
-		if RoundManager.gamemode == "Arena":
-			get_tree().change_scene("res://arena-mode/Arena.tscn")
-		elif RoundManager.gamemode == "Race":
-			get_tree().change_scene("res://race-mode/SplitScreen2.tscn")
+func _on_box_readied():
+	for box in boxes:
+		if not box.is_closed() and not box.is_ready():
+			return
+
+	# Go to next screen
--- project/character-select/SelectionBox.gd
+++ project/character-select/SelectionBox.gd
@@ -1,78 +1,108 @@
 extends Control
 
-signal unselected(id)
-signal selected(id)
-signal ready(id)
-signal unready(id)
-signal start_game
+signal selected(character)
+signal unselected(character)
+signal readied
 
-const characters = [Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)]
-enum States {UNSELECTED, SELECTED, READY}
+enum States {CLOSED, OPEN, READY}
 
-export(int, 0, 3) var id = 0
+const CHARACTERS = ["a", "b", "c", "d"]
 
-var current_state = States.UNSELECTED
-var current_character_id = 0
-
+var available_chars = CHARACTERS.duplicate()
+var char_index
+var device_name = ""
+var state = States.CLOSED
 
 func _ready():
-	set_process_input(false)
+	pass
 
 
 func _input(event):
-	if not event is InputEventKey and not event is InputEventJoypadButton:
+	if get_device_name_from(event) != device_name:
 		return
-	var device = determine_device_type(event)
 	
-	match current_state:
-		UNSELECTED:
-			if event.is_action_pressed("start"):
-				if not device in RoundManager.control_map:
-					if(RoundManager.assign_to_port(device)):
-						change_state(SELECTED, device)
-		SELECTED:
-			if not device == RoundManager.control_map[id]:
-				return
-			if event.is_action_pressed("ui_cancel"):
-				change_state(UNSELECTED, device)
-			elif event.is_action_pressed("ui_accept"):
-				change_state(READY, device)
-			elif event.is_action_pressed("ui_left"):
-				current_character_id -= 1
-			elif event.is_action_pressed("ui_right"):
-				current_character_id += 1
-			current_character_id %= characters.size()
-			$Sprite.set_modulate(characters[current_character_id])
-		READY:
-			if not device == RoundManager.control_map[id]:
-				return
-			if event.is_action_pressed("ui_cancel"):
-				emit_signal("unselected", id)
-				change_state(SELECTED, device)
-			elif event.is_action_pressed("ui_accept"):
-				emit_signal("start_game")
+	if event.is_action_pressed("ui_start"):
+		if state == States.OPEN and CHARACTERS[char_index] in available_chars:
+			change_state(States.READY)
+		emit_signal("readied")
+	
+	elif event.is_action_pressed("ui_cancel"):
+		if state == States.OPEN:
+			device_name = ""
+			change_state(States.CLOSED)
+		elif state == States.READY:
+			change_state(States.OPEN)
+	
+	elif event.is_action_pressed("ui_left") and state == States.OPEN:
+		set_character(char_index - 1)
+	
+	elif event.is_action_pressed("ui_right") and state == States.OPEN:
+		set_character(char_index + 1)
+		pass
+	
+	get_tree().set_input_as_handled()
 
 
-func determine_device_type(event):
-	var device
+func change_state(new_state):
+	match new_state:
+		States.CLOSED:
+			device_name = ""
+			$State.set_text("CLOSED")
+			$Device.set_text("")
+		States.OPEN:
+			$State.set_text("OPEN")
+			if state == States.READY:
+				emit_signal("unselected", CHARACTERS[char_index])
+		States.READY:
+			$State.set_text("READY")
+			emit_signal("selected", CHARACTERS[char_index])
+	
+	state = new_state
+
+
+func is_closed():
+	return state == States.CLOSED
+
+
+func is_ready():
+	return state == States.READY
+
+
+func open_with(event):
+	device_name = get_device_name_from(event)
+	$Device.set_text(str(device_name))
+	change_state(States.OPEN)
+
+
+func update_available_characters(characters):
+	available_chars = characters
+	print(str(self.get_name(), " ", available_chars, " ", char_index, " ", CHARACTERS))
+	set_character(char_index)
+
+
+func set_character(index):
+	char_index = wrapi(index, 0, CHARACTERS.size())
+	$Character.set_text(str(char_index))
+	# Change image here
+	
+	if not CHARACTERS[char_index] in available_chars:
+		# Display as unavailable
+		pass
+
+
+
+
+
+
+
+
+
+
+func get_device_name_from(event):
 	if event is InputEventKey:
-		device = 'keyboard'
-	else:
-		device = str("gamepad_", event.device)
-	return device
+		return "keyboard"
 	
-
-func change_state(new_state, device):
-	current_state = new_state
-	$State.text = str(new_state)
-	$DeviceId.text = str(device)
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		return str("gamepad_", event.device)
 	
-	match current_state:
-		UNSELECTED:
-			RoundManager.unassign_port(device)
-			set_process_input(false)
-			emit_signal("unselected", id)
-		SELECTED:
-			emit_signal("selected", id)
-		READY:
-			emit_signal("ready", id)
+	return "invalid_device"
