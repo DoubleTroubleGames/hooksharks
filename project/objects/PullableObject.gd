1ffc5dbbb9a513891d3f0ad0f5b5777df284c9d1
Detril <andremoukarzel@gmail.com>
2019-02-28 17:15:24 -0300

    Add signals to PullableObject and make FinishLine update when pulled

--- project/objects/PullableObject.gd
+++ project/objects/PullableObject.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
+signal hooked
+signal unhooked
+
 var hook
 
+
+func _ready():
+	set_physics_process(false)
+
 func _physics_process(delta):
-	if hook:
-		position = hook.position
+	position = hook.position
 
 func setHook(new_hook):
 	if hook:
 		hook.retract()
 	hook = new_hook
+	set_physics_process(true)
 
 func removeHook():
 	hook = null
+	set_physics_process(false)
--- project/race-mode/FinishLine.gd
+++ project/race-mode/FinishLine.gd
@@ -6,6 +6,8 @@ export (int)var last_checkpoint_number
 export (int)var total_laps = 1
 
 var line_polygon
+var top_pulling = false
+var bot_pulling = false
 
 
 func _ready():
@@ -17,8 +19,22 @@ func _draw():
 	var color_array = [Color(1, 1, 1), Color(0, 0, 0), Color(1, 1, 1), Color(0, 0, 0)]
 	draw_polygon(line_polygon, color_array)
 
+func _physics_process(delta):
+	update_line_polygon()
+	update()
+	$LineArea/CollisionPolygon2D.polygon = line_polygon
 
-func _on_Area2D_area_entered(area):
+
+func update_line_polygon():
+	var PullTop_pos = $PullableObjectTop.get_position()
+	var PullBot_pos = $PullableObjectBot.get_position()
+	line_polygon = PoolVector2Array([Vector2(PullTop_pos.x - WIDTH, PullTop_pos.y),
+								   Vector2(PullTop_pos.x + WIDTH, PullTop_pos.y),
+								   Vector2(PullBot_pos.x + WIDTH, PullBot_pos.y),
+								   Vector2(PullBot_pos.x - WIDTH, PullBot_pos.y)])
+
+
+func _on_LineArea_area_entered(area):
 	if area.get_parent().is_in_group('player'):
 		var player = area.get_parent()
 		if player.checkpoint_number == last_checkpoint_number:
@@ -34,10 +50,23 @@ func _on_Area2D_area_entered(area):
 		player.checkpoint_number = 0
 
 
-func update_line_polygon():
-	var PullTop_pos = $PullableObjectTop.get_position()
-	var PullBot_pos = $PullableObjectBot.get_position()
-	line_polygon = PoolVector2Array([Vector2(PullTop_pos.x - WIDTH, PullTop_pos.y),
-								   Vector2(PullTop_pos.x + WIDTH, PullTop_pos.y),
-								   Vector2(PullBot_pos.x + WIDTH, PullBot_pos.y),
-								   Vector2(PullBot_pos.x - WIDTH, PullBot_pos.y)])
+func _on_PullableObjectTop_hooked():
+	set_physics_process(true)
+	top_pulling = true
+
+
+func _on_PullableObjectBot_hooked():
+	set_physics_process(true)
+	bot_pulling = true
+
+
+func _on_PullableObjectTop_unhooked():
+	top_pulling = false
+	if not top_pulling and not bot_pulling:
+		set_physics_process(false)
+
+
+func _on_PullableObjectBot_unhooked():
+	bot_pulling = false
+	if not top_pulling and not bot_pulling:
+		set_physics_process(false)
