bb79e7c56e282213151bff6e39210e9e957ab7a7
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2019-03-01 13:28:08 -0300

    Make small fixes to character select

--- project/hud/character-select/CharacterSelect.gd
+++ project/hud/character-select/CharacterSelect.gd
@@ -6,12 +6,13 @@ var available_characters
 var starting_game = false
 
 func _ready():
+	RoundManager.device_map.clear()
+	
 	available_characters = $Boxes/SelectionBox0.CHARACTERS.duplicate()
 
 	for i in range(boxes.size()):
 		boxes[i].connect("selected", self, "_on_box_selected")
 		boxes[i].connect("unselected", self, "_on_box_unselected")
-		boxes[i].connect("readied", self, "_on_box_readied")
 		boxes[i].connect("tried_to_start", self, "_on_box_tried_to_start")
 		boxes[i].set_character(0)
 
@@ -30,9 +31,11 @@ func _input(event):
 func update_boxes():
 	for box in boxes:
 		box.update_available_characters(available_characters)
+	
+	try_start_message()
 
 
-func update_device_map():
+func set_device_map():
 	for box in boxes:
 		if box.is_ready():
 			RoundManager.device_map.append(box.device_name)
@@ -60,12 +63,21 @@ func start_game():
 	
 	starting_game = true
 	
-	update_device_map()
+	set_device_map()
 	
 	if RoundManager.gamemode == "Arena":
 		get_tree().change_scene("res://arena-mode/Arena.tscn")
 	elif RoundManager.gamemode == "Race":
 		get_tree().change_scene("res://race-mode/Race.tscn")
+
+
+func try_start_message():
+	if can_start():
+		# Show "press start to begin message"
+		pass
+	else:
+		# Hide "press start to begin message"
+		pass
 
 
 func _on_box_selected(character):
@@ -76,12 +88,6 @@ func _on_box_selected(character):
 func _on_box_unselected(character):
 	available_characters.append(character)
 	update_boxes()
-
-
-func _on_box_readied():
-	if can_start():
-		# Show "press start to begin" message
-		pass
 
 
 func _on_box_tried_to_start():
