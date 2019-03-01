22fa7f0687e4cd8f6561c1022d525a31192e04c3
Detril <andremoukarzel@gmail.com>
2019-03-01 12:42:11 -0300

    Makes Stages.gd handle checkpoints on race
    Closes #65

--- project/race-mode/FinishLine.gd
+++ project/race-mode/FinishLine.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+onready var Stage = get_parent()
+
 const WIDTH = 10
 
-export (int)var last_checkpoint_number
+export (int)var total_checkpoint_number = 0
 export (int)var total_laps = 1
 
 var line_polygon
@@ -36,18 +38,21 @@ func update_line_polygon():
 
 func _on_LineArea_area_entered(area):
 	if area.get_parent().is_in_group('player'):
-		var player = area.get_parent()
-		if player.checkpoint_number == last_checkpoint_number:
-			player.lap += 1
-			if player.lap >= total_laps:
-				var winner = player
+		var Player = area.get_parent()
+		var checkpoint_num = Stage.get_player_checkpoint(Player)
+		if checkpoint_num == total_checkpoint_number:
+			Stage.inscrease_player_lap(Player)
+			Stage.reset_player_checkpoint(Player)
+			
+			var lap_num = Stage.get_player_lap(Player)
+			if lap_num >= total_laps:
+				var winner = Player
 				var players = winner.get_parent()
 				
 				$LineArea/CollisionPolygon2D.disabled = true
 				for child in players.get_children():
 					if child.is_in_group('player') and child != winner:
 						child._queue_free()
-		player.checkpoint_number = 0
 
 
 func _on_PullableObjectTop_hooked():
