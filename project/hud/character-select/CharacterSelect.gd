906a2f28dd3de4d4d9433cbf43798123830f7a24
rilifon <pudimdefrutas@gmail.com>
2019-03-01 14:12:40 -0300

    Back button for character screen

--- project/hud/character-select/CharacterSelect.gd
+++ project/hud/character-select/CharacterSelect.gd
@@ -1,11 +1,16 @@
 extends Control
 
 onready var boxes = $Boxes.get_children()
+onready var bar = $BackIndicator/bar
 
 var available_characters
 var starting_game = false
+var back_indicator_up_speed = 100
+var back_indicator_down_speed = 150
+var trying_to_leave
 
 func _ready():
+	trying_to_leave = false
 	available_characters = $Boxes/SelectionBox0.CHARACTERS.duplicate()
 
 	for i in range(boxes.size()):
@@ -15,17 +20,33 @@ func _ready():
 		boxes[i].connect("tried_to_start", self, "_on_box_tried_to_start")
 		boxes[i].set_character(0)
 
+func _physics_process(delta):
+	if trying_to_leave:
+		bar.value = min(100, bar.value + back_indicator_up_speed*delta) 
+	else:
+		bar.value = max(0, bar.value - back_indicator_down_speed*delta) 
+	if bar.value >= 100:
+		RoundManager.gamemode = "None"
+		get_tree().change_scene("res://main-menu/MainMenu.tscn")	
+	elif bar.value > 0:
+		bar.visible = true
+	else:
+		bar.visible = false
+	
+
 
 func _input(event):
 	if event.is_action_pressed("ui_start"):
+		trying_to_leave = false
 		for box in boxes:
 			if box.is_closed():
 				box.open_with(event)
 				return
 	elif event.is_action_pressed("ui_cancel"):
-		# Go to previous screen
-		pass
-
+		trying_to_leave = true
+	elif event.is_action_released("ui_cancel"):
+		trying_to_leave = false
+		
 
 func update_boxes():
 	for box in boxes:
