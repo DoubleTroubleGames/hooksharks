66380f7e9f2cc8fac9d87fe103293bb6f957ced3
Detril <andremoukarzel@gmail.com>
2019-03-07 17:03:21 -0300

    Add sizable level walls

--- project/obstacles/Wall.gd
+++ project/obstacles/Wall.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 export (int)var size = 1000 setget set_size
 
-var objects = ['bigbox', 'box', 'box2', 'box3', 'boxmetal', 'float2']
+var objects = ['Box', 'Roof', 'Tire']
 
 
 #func _ready():
@@ -15,4 +15,11 @@ func set_size(s):
 	size = s
 	for child in get_children():
 		child.queue_free()
-	
+	randomize()
+	var current_size = 0
+	while current_size < size:
+		var Obj = load(str("res://obstacles/", objects[randi() % objects.size()],".tscn")).instance()
+		var obj_size = Obj.get_node("Sprite").texture.get_size().x
+		Obj.set_position(Vector2(current_size + obj_size/2, 0))
+		add_child(Obj)
+		current_size += obj_size
