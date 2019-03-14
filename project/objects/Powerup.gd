bff5a21787f045028e60a807d211b2732c7fed12
LuizGyro <luizfelipe.g@hotmail.com>
2019-03-14 17:27:59 -0300

    Added powerup box behaviour Now the box can generate powers for itself, if it is not done so beforehand (a.k.a. setting it on the scene editor). It generate random ones, for now. It respawns after it is collected, with another random powerup.

--- project/objects/Powerup.gd
+++ project/objects/Powerup.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
 export (PackedScene)var powerup
+
+const POWERS = [preload("res://objects/Powerups/MegaHook.tscn"),
+                preload("res://objects/Powerups/TrailPower.tscn")]
+
 var hook
+
+func _ready():
+	if (powerup == null):
+		set_random_power()
 
 func _physics_process(delta):
 	if hook:
@@ -14,7 +22,20 @@ func setHook(new_hook):
 
 func removeHook():
 	hook = null
+	
+func despawn():
+	$Hitbox/CollisionShape2D.set_disabled(true)
+	hide()
 
+func spawn():
+	set_random_power()
+	$Hitbox/CollisionShape2D.set_disabled(false)
+	show()
+	
+func set_random_power():
+	var index = randi() % POWERS.size()
+	powerup = POWERS[index]
+	
 func activate(player):
 	var power = powerup.instance()
 	# If initialized sucessfully, add it to player
@@ -22,5 +43,7 @@ func activate(player):
 		player.get_node("PowerUps").add_child(power)
 	if hook:
 		hook.free_hook()
-	queue_free()
+	
+	despawn()
+	$RespawnTimer.start()
 	
