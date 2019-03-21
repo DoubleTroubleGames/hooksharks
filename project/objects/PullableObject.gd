b3ea6839f16f10d84343d7f69041fc54eb61eb1c
Detril <andremoukarzel@gmail.com>
2019-03-21 15:07:09 -0300

    Adjust Hook's pull timers
    Fix #76

--- project/objects/PullableObject.gd
+++ project/objects/PullableObject.gd
@@ -14,6 +14,12 @@ func setHook(new_hook):
 	if hook:
 		hook.retract()
 	hook = new_hook
+	$Timer.start()
 
 func removeHook():
 	hook = null
+
+func _on_Timer_timeout():
+	if hook:
+		hook.retract()
+		removeHook()
