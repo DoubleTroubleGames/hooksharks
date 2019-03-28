63484a5617d01859d62354f32eac54a8b357bcf1
Detril <andremoukarzel@gmail.com>
2019-03-28 19:10:17 -0300

    Finish FinishLine

--- project/race-mode/FinishLine.gd
+++ project/race-mode/FinishLine.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var Stage = get_parent()
 
 const WIDTH = 10
+const LINE_SPRITE_SIZE = 115
 
 export (int)var total_checkpoint_number = 0
 export (int)var total_laps = 1
@@ -16,20 +17,14 @@ var line_tex = preload("res://assets/line.png")
 func _ready():
 	update_line_polygon()
 	rotate_line()
-	add_line_sprites()
-	$LineArea/CollisionPolygon2D.polygon = line_polygon
+	adjust_line_size()
 	set_physics_process(false)
-
-
-func _draw():
-	var color_array = [Color(1, 1, 1), Color(0, 0, 0), Color(1, 1, 1), Color(0, 0, 0)]
-	draw_polygon(line_polygon, color_array)
 
 
 func _physics_process(delta):
 	update_line_polygon()
-	update()
 	rotate_line()
+	adjust_line_size()
 
 
 func update_line_polygon():
@@ -46,28 +41,11 @@ func rotate_line():
 	var angle = $PullableObjectTop.position.angle_to_point($PullableObjectBot.position)
 	$PullableObjectTop.rotation = angle
 	$PullableObjectBot.rotation = angle
-	$LineSprites.rotation = angle
-	$LineSprites.position = $PullableObjectBot.position
 
 
-func add_line_sprites():
+func adjust_line_size():
 	var distance = abs($PullableObjectTop.position.distance_to($PullableObjectBot.position))
-	var pos = 57.5
-	
-	while distance > 0:
-		var sprite = Sprite.new()
-		
-		sprite.texture = line_tex
-		sprite.position = Vector2(pos, 0)
-		if distance < 115:
-			var rect = Rect2(Vector2(0, 0), Vector2(distance, 115))
-			sprite.region_enabled = true
-			sprite.region_rect = rect
-			sprite.position.x += 115 - distance
-		$LineSprites.add_child(sprite)
-		
-		pos += 115
-		distance -= 115
+	$PullableObjectBot/Line.rect_size.x = distance
 
 
 func _on_LineArea_area_entered(area):
