a4553887a69d29a5f0deea7368761d787df84f8e
Detril <andremoukarzel@gmail.com>
2019-03-29 11:03:16 -0300

    Make so FinishLine can't be pullable together
    Fix #88

--- project/objects/PullableObject.gd
+++ project/objects/PullableObject.gd
@@ -25,3 +25,14 @@ func _on_Timer_timeout():
 		emit_signal("unhooked")
 		hook.retract()
 		removeHook()
+
+
+func _on_Hitbox_area_entered(area):
+	if area.get_parent().get_parent() == self.get_parent():
+		var dist = self.get_global_position() - area.get_global_position()
+		var diff = 115 - min(dist.length(), 115)
+		
+		self.set_global_position(self.get_global_position() + diff * dist.normalized())
+		print(self.global_position)
+		if hook:
+			hook.retract()
