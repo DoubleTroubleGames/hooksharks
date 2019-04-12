4e16ee27cf4eeb710650e1ceb44cfea04287b87c
Detril <andremoukarzel@gmail.com>
2019-04-12 11:41:24 -0300

    Add smoother Shark changes and fix visual errors in char change

--- project/hud/character-select/SelectionBox.gd
+++ project/hud/character-select/SelectionBox.gd
@@ -16,6 +16,7 @@ var state = States.CLOSED
 var _moved_left = false
 var _moved_right = false
 var changing = false
+var rechange_shark = false
 
 
 func _ready():
@@ -51,7 +52,7 @@ func _input(event):
 				change_state(States.READY)
 				$Sounds/ConfirmSFX.play()
 			else:
-				$Sprite/AnimationPlayer.play("shake")
+				$AnimationPlayer.play("shake")
 				$Sounds/CancelSFX.play()
 		elif state == States.READY:
 			$AnimationPlayer.play("shake")
@@ -82,14 +83,10 @@ func toggle_left():
 	set_character(char_index - 1)
 	#### Visuals for character changing ####
 	changing = true
-	$SharkSprite/Shark/AnimationPlayer.play("dive")
+	change_shark()
 	$Boarder/Portrait.set_texture(load(str("res://hud/character-select/", CHARACTERS[char_index], ".png")))
 	$Boarder/AnimationPlayer.play("change_char_left")
 	yield($Boarder/AnimationPlayer, "animation_finished")
-	add_shark(CHARACTERS[char_index])
-	$SharkSprite/Shark/AnimationPlayer.play("emerge")
-	yield($SharkSprite/Shark/AnimationPlayer, "animation_finished")
-	$SharkSprite/Shark/AnimationPlayer.play("idle")
 	changing = false
 	########################################
 	$Sounds/SelectSFX.play()
@@ -100,14 +97,10 @@ func toggle_right():
 	set_character(char_index + 1)
 	#### Visuals for character changing ####
 	changing = true
-	$SharkSprite/Shark/AnimationPlayer.play("dive")
+	change_shark()
 	$Boarder/Portrait.set_texture(load(str("res://hud/character-select/", CHARACTERS[char_index], ".png")))
 	$Boarder/AnimationPlayer.play("change_char_right")
 	yield($Boarder/AnimationPlayer, "animation_finished")
-	add_shark(CHARACTERS[char_index])
-	$SharkSprite/Shark/AnimationPlayer.play("emerge")
-	yield($SharkSprite/Shark/AnimationPlayer, "animation_finished")
-	$SharkSprite/Shark/AnimationPlayer.play("idle")
 	changing = false
 	########################################
 	$Sounds/SelectSFX.play()
@@ -183,6 +176,29 @@ func add_shark(shark_name):
 
 func shake():
 	$AnimationPlayer.play("shake")
+
+
+func change_shark():
+	var shark_anim = $SharkSprite/Shark/AnimationPlayer
+	
+	if shark_anim.current_animation != "idle": # shark already changing
+		rechange_shark = true
+		return
+	
+	shark_anim.play("dive")
+	yield(shark_anim, "animation_finished")
+	add_shark(CHARACTERS[char_index])
+	shark_anim = $SharkSprite/Shark/AnimationPlayer # since the shark scene was change, we have to update this node
+	shark_anim.play("dive_idle")
+	yield(get_tree().create_timer(shark_anim.current_animation_length/2), "timeout")
+	shark_anim.play("emerge")
+	shark_anim.queue("idle")
+	
+	if rechange_shark:
+		yield(get_tree().create_timer(shark_anim.current_animation_length/2), "timeout")
+		rechange_shark = false
+		shark_anim.play("idle")
+		change_shark()
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
