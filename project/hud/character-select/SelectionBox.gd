9da1ee88895c3c18d1b99a2632e9e7d22fa2dc76
rilifon <pudimdefrutas@gmail.com>
2019-04-24 18:17:28 -0300

    Fixed character select box input conflicts

--- project/hud/character-select/SelectionBox.gd
+++ project/hud/character-select/SelectionBox.gd
@@ -16,7 +16,7 @@ var device_name = ""
 var state = States.CLOSED
 var _moved_left = false
 var _moved_right = false
-var changing = false
+var mid_animation = false
 
 
 func _ready():
@@ -27,10 +27,10 @@ func _physics_process(delta):
 	if device_name.left(8) == "gamepad_":
 		var device_n = int(device_name.right(8))
 		var axis_value = Input.get_joy_axis(device_n, 0)
-		if axis_value >= DEADZONE and not _moved_right and not changing:
+		if axis_value >= DEADZONE and state == States.OPEN and  not _moved_right and not mid_animation:
 			_moved_right = true
 			toggle_left()
-		elif  axis_value <= -DEADZONE and not _moved_left and not changing:
+		elif  axis_value <= -DEADZONE and state == States.OPEN and not _moved_left and not mid_animation:
 			_moved_left = true
 			toggle_right()
 		elif abs(axis_value) < DEADZONE:
@@ -42,7 +42,7 @@ func _input(event):
 	if RoundManager.get_device_name_from(event) != device_name:
 		return
 	
-	if event.is_action_pressed("ui_start"):
+	if event.is_action_pressed("ui_start") and not mid_animation:
 		if state == States.OPEN:
 			if CHARACTERS[char_index] in available_chars:
 				change_state(States.READY)
@@ -52,20 +52,24 @@ func _input(event):
 		elif state == States.READY:
 			emit_signal("tried_to_start")
 
-	elif event.is_action_pressed("ui_cancel"):
+	elif event.is_action_pressed("ui_cancel") and not mid_animation:
 		if state == States.OPEN:
 			device_name = ""
 			change_state(States.CLOSED)
 			$Sounds/CancelSFX.play()
 		elif state == States.READY:
+			mid_animation = true
 			$Boarder/AnimationPlayer.play("unready")
 			change_state(States.OPEN)
 			$Sounds/CancelSFX.play()
+			yield($Boarder/AnimationPlayer, "animation_finished")
+			mid_animation = false
+			
 
-	elif event.is_action_pressed("ui_left") and state == States.OPEN and not changing:
+	elif event.is_action_pressed("ui_left") and state == States.OPEN and not mid_animation:
 		toggle_left()
 
-	elif event.is_action_pressed("ui_right") and state == States.OPEN and not changing:
+	elif event.is_action_pressed("ui_right") and state == States.OPEN and not mid_animation:
 		toggle_right()
 
 	get_tree().set_input_as_handled()
@@ -75,28 +79,28 @@ func toggle_left():
 	$Boarder/ChangePortrait.set_texture(load(str("res://characters/", CHARACTERS[char_index], "/portrait.png")))
 	set_character(char_index - 1)
 	change_shark()
+	$Sounds/SelectSFX.play()
 	#### Visuals for character changing ####
-	changing = true
+	mid_animation = true
 	$Boarder/Portrait.set_texture(load(str("res://characters/", CHARACTERS[char_index], "/portrait.png")))
 	$Boarder/AnimationPlayer.play("change_char_left")
 	yield($Boarder/AnimationPlayer, "animation_finished")
-	changing = false
+	mid_animation = false
 	########################################
-	$Sounds/SelectSFX.play()
 
 
 func toggle_right():
 	$Boarder/ChangePortrait.set_texture(load(str("res://characters/", CHARACTERS[char_index], "/portrait.png")))
 	set_character(char_index + 1)
 	change_shark()
+	$Sounds/SelectSFX.play()
 	#### Visuals for character changing ####
-	changing = true
+	mid_animation = true
 	$Boarder/Portrait.set_texture(load(str("res://characters/", CHARACTERS[char_index], "/portrait.png")))
 	$Boarder/AnimationPlayer.play("change_char_right")
 	yield($Boarder/AnimationPlayer, "animation_finished")
-	changing = false
+	mid_animation = false
 	########################################
-	$Sounds/SelectSFX.play()
 
 
 func change_state(new_state):
@@ -109,6 +113,9 @@ func change_state(new_state):
 			$Boarder/Right.hide()
 			$SharkSprite.hide()
 			$Boarder/AnimationPlayer.play("close")
+			mid_animation = true
+			yield($Boarder/AnimationPlayer, "animation_finished")
+			mid_animation = false
 		States.OPEN:
 			$SharkSprite.show()
 			$Boarder/Left.show()
@@ -117,7 +124,10 @@ func change_state(new_state):
 				emit_signal("unselected", CHARACTERS[char_index])
 		States.READY:
 			emit_signal("selected", CHARACTERS[char_index])
+			mid_animation = true
 			$Boarder/AnimationPlayer.play("ready")
+			yield($Boarder/AnimationPlayer, "animation_finished")
+			mid_animation = false
 
 	state = new_state
 
@@ -144,8 +154,11 @@ func open_with(event):
 		$Boarder/DeviceNumber.set_text(str(num))
 
 	$Boarder/AnimationPlayer.play("open")
+	mid_animation = true
 	$Sounds/ConfirmSFX.play()
 	change_state(States.OPEN)
+	yield($Boarder/AnimationPlayer, "animation_finished")
+	mid_animation = false
 
 
 func update_available_characters(characters):
