cb9612f0ecb9966e9d231091e03bf33c4582a124
ArthurTui <arthur.vb@gmail.com>
2019-05-10 16:44:58 -0300

    Reorganize folders and fix broken dependencies

---
+++ project/gameplay/arena-mode/Arena.gd
@@ -0,0 +1,19 @@
+extends "res://gameplay/Game.gd"
+
+func get_cameras():
+	return [$Camera2D]
+
+func connect_players():
+	for player in players:
+		player.connect("created_trail", self, "_on_player_created_trail")
+		player.connect("hook_shot", self, "_on_player_hook_shot")
+		player.connect("megahook_shot", self, "_on_player_megahook_shot")
+		player.connect("died", self, "remove_player")
+		player.connect("paused", self, "_on_player_paused")
+		for camera in Cameras:
+			player.connect("shook_screen", camera, "add_shake")
+
+func activate_players():
+	for player in players:
+		player.respawn = false
+		player.enable()
---
+++ project/menus/mode-select/ModeSelect.gd
@@ -0,0 +1,76 @@
+extends Control
+
+onready var _moved_left = false
+onready var _moved_right = false
+
+func _ready():
+	set_process(true)
+	set_process_input(false)
+	
+	Transition.transition_out()
+	yield(Transition, "finished")
+	
+	set_process_input(true)
+	$ArenaButton.disabled = false
+	$RacingButton.disabled = false
+	$ArenaButton.grab_focus()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		transition_to("res://menus/main-menu/MainMenu.tscn")
+
+func _process(delta):
+	if not _moved_left and Input.is_action_just_pressed("ui_joy_left"):
+		_moved_left = true
+		var cur_focus = self.get_focus_owner()
+		if cur_focus and cur_focus.focus_neighbour_left:
+			cur_focus.get_node(cur_focus.focus_neighbour_left).grab_focus()
+	if not _moved_right and Input.is_action_just_pressed("ui_joy_right"):
+		_moved_right = true
+		var cur_focus = self.get_focus_owner()
+		if cur_focus and cur_focus.focus_neighbour_right:
+			cur_focus.get_node(cur_focus.focus_neighbour_right).grab_focus()
+	if _moved_left and Input.is_action_just_released("ui_joy_left"):
+		_moved_left = false
+	if _moved_right and Input.is_action_just_released("ui_joy_right"):
+		_moved_right = false
+
+func transition_to(scene_path):
+	set_process_input(false)
+	$ArenaButton.disabled = true
+	$RacingButton.disabled = true
+	
+	Transition.transition_in()
+	yield(Transition, "finished")
+	get_tree().change_scene(scene_path)
+
+
+func _on_ArenaButton_pressed():
+	RoundManager.gamemode = "Arena"
+	$Sounds/ConfirmSFX.play()
+	transition_to("res://menus/character-select/CharacterSelect.tscn")
+
+
+func _on_RacingButton_pressed():
+	RoundManager.gamemode = "Race"
+	$Sounds/ConfirmSFX.play()
+	transition_to("res://menus/character-select/CharacterSelect.tscn")
+
+
+func _on_ArenaButton_focus_entered():
+	$ArenaButton.material.set_shader_param("active", true)
+	$Sounds/SelectSFX.play()
+
+
+func _on_ArenaButton_focus_exited():
+	$ArenaButton.material.set_shader_param("active", false)
+
+
+func _on_RacingButton_focus_entered():
+	$RacingButton.material.set_shader_param("active", true)
+	$Sounds/SelectSFX.play()
+
+
+func _on_RacingButton_focus_exited():
+	$RacingButton.material.set_shader_param("active", false)
