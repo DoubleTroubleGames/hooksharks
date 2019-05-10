32e5110b3e606168b3ca29675e029cd7685cb67d
LuizGyro <luizfelipe.g@hotmail.com>
2019-05-10 16:28:24 -0300

    Update MovingObstacle.gd

--- project/objects/obstacles/dynamic/MovingObstacle.gd
+++ project/objects/obstacles/dynamic/MovingObstacle.gd
@@ -11,7 +11,7 @@ export(int, "Linear", "Sine", "Quintic", "Quartic", "Quadratic",\
        "Exponential", "Elastic", "Cubic") var transition_type
 export(int, "In", "Out", "In-Out", "Out-In") var ease_type
 export(float) var duration = 1.0
-export(bool) var explicit_loop = true
+export(bool) var explicit_loop = true setget define_loop_type
 export(bool) var playtest = false setget test_move
 
 var root_position = self.position
@@ -25,9 +25,11 @@ func _ready():
 	if (!explicit_loop):
 		var loop_end = Position2D.new()
 		loop_end.position = $Path/Origin.position
+		loop_end.name = "ImplicitLoop"
 		$Path.add_child(loop_end)
 	previous_position = $Path/Origin.position
-	set_process(true)
+	# This is done so tool mode does not activate process on its own
+	set_process(!Engine.is_editor_hint())
 
 func _process(delta):
 	if(!moving):
@@ -46,22 +48,25 @@ func move():
 	
 func real_position(p):
 	return p + root_position
-	
-func test_move(do):
-	if (Engine.is_editor_hint() and do):
-		playtest = true
-		if (!explicit_loop):
-			var loop_end = Position2D.new()
-			loop_end.position = $Path/Origin.position
-			$Path.add_child(loop_end)
-		previous_position = $Path/Origin.position
-		root_position = self.position
+
+
+
+
+
+func define_loop_type(explicit):
+	if (explicit):
 		for node in $Path.get_children():
-			if node.get_name() != "Origin":
-				$Tween.interpolate_property(self, "position", real_position(previous_position),\
-				       real_position(node.position), duration, transition_type, ease_type)
-				$Tween.start()
-				yield($Tween, "tween_completed")
-				previous_position = node.position
-		playtest = false
+			if node.name == "ImplicitLoop":
+				$Path.remove_child(node)
+	else:
+		var loop_end = Position2D.new()
+		loop_end.position = $Path/Origin.position
+		loop_end.name = "ImplicitLoop"
+		$Path.add_child(loop_end)
+	explicit_loop = explicit
+
+func test_move(do):
+	print(do)
+	set_process(do)
+	playtest = do
 
