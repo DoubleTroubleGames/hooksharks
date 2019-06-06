93c9339af3d3ed326057bdaf707181da09e633bb
Detril <andremoukarzel@gmail.com>
2019-06-06 17:20:24 -0300

    Add Resolution options

--- project/menus/options-menu/OptionsMenu.gd
+++ project/menus/options-menu/OptionsMenu.gd
@@ -1,17 +1,29 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var resolutions = ['1920x1080', '1440x900', '1366x768', '1280x800']
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	$Fullscreen.pressed = OS.window_fullscreen
+	var native_size = OS.get_screen_size()
+	native_size = str(native_size.x, 'x', native_size.y)
+	
+	$Resolution/Fullscreen.pressed = OS.window_fullscreen
+	if not native_size in resolutions:
+		resolutions.append(native_size)
+	for res in resolutions:
+		$Resolution/ScreenSize.add_item(res)
+	$Resolution/ScreenSize.selected = resolutions.find(native_size)
 
 
 func _on_Fullscreen_toggled(button_pressed):
 	OS.window_fullscreen = button_pressed
+	if button_pressed:
+		var native_res = OS.get_screen_size()
+		$Resolution/ScreenSize.selected = resolutions.find(str(native_res.x, 'x', native_res.y))
 
+func _on_ScreenSize_item_selected(ID):
+	var new_res = $Resolution/ScreenSize.get_item_text(ID)
+	OS.window_size = Vector2(int(new_res.split('x')[0]), int(new_res.split('x')[1]))
 
 func _on_MasterVolume_value_changed(value):
 	var volume = float(value)/100.0
