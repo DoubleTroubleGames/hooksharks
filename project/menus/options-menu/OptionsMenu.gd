2f2ef5ff62a57f9565c1761f2c44ad79ebb43aa5
Arthur Vieira Barbosa <arthur.vb@gmail.com>
2020-02-21 13:30:28 -0300

    Remove weird line in OptionsMenu.gd

--- project/menus/options-menu/OptionsMenu.gd
+++ project/menus/options-menu/OptionsMenu.gd
@@ -8,10 +8,11 @@ onready var SoundMaster = $Sound/Float/VScrollBar/MasterVolume/MasterVolume
 onready var SoundSFX = $Sound/Float/VScrollBar/SFXVolume/SFXVolume
 onready var SoundBGM = $Sound/Float/VScrollBar/BGMVolume/BGMVolume
 
+enum {MASTER, SFX, BGM}
+
 var resolutions = ['1920x1080', '1440x900', '1366x768', '1280x800']
 var back_indicator_up_speed = 100
 var back_indicator_down_speed = 150
-
 
 func _ready():
 	var native_size = OS.get_screen_size()
@@ -21,9 +22,9 @@ func _ready():
 	yield(Transition, "finished")
 	
 	FullscreenButton.pressed = OS.window_fullscreen
-	SoundMaster.value = 100 * db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
-	SoundSFX.value = 100 * db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
-	SoundBGM.value = 100 * db2linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("BGM")))
+	SoundMaster.value = 100 * db2linear(AudioServer.get_bus_volume_db(MASTER))
+	SoundSFX.value = 100 * db2linear(AudioServer.get_bus_volume_db(SFX))
+	SoundBGM.value = 100 * db2linear(AudioServer.get_bus_volume_db(BGM))
 	if not native_size in resolutions:
 		resolutions.append(native_size)
 	for res in resolutions:
@@ -58,19 +59,22 @@ func _on_Fullscreen_toggled(button_pressed):
 		var native_res = OS.get_screen_size()
 		ScreenSizeButton.selected = resolutions.find(str(native_res.x, 'x', native_res.y))
 
+
 func _on_ScreenSize_item_selected(ID):
 	var new_res = ScreenSizeButton.get_item_text(ID)
 	OS.window_size = Vector2(int(new_res.split('x')[0]), int(new_res.split('x')[1]))
 
+
 func _on_MasterVolume_value_changed(value):
 	var volume = float(value)/100.0
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(volume))
+	AudioServer.set_bus_volume_db(MASTER, linear2db(volume))
+
 
 func _on_SFXVolume_value_changed(value):
 	var volume = float(value)/100.0
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear2db(volume))
+	AudioServer.set_bus_volume_db(SFX, linear2db(volume))
+
 
 func _on_BGMVolume_value_changed(value):
 	var volume = float(value)/100.0
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), linear2db(volume))
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), linear2db(volume * 0.9))
+	AudioServer.set_bus_volume_db(BGM, linear2db(volume))
